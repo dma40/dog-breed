2277ea1173a09922c0aba92c25e45f9d5cbc4922
dma40 <dma99878@gmail.com>
2025-10-25 17:17:21 -0400

    change BreedNotFoundException to be checked

--- src/test/java/dogapi/MainTest.java
+++ src/test/java/dogapi/MainTest.java
@@ -7,20 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void getNumberOfSubBreedsValidBreed() {
+    void getNumberOfSubBreedsValidBreed() throws BreedFetcher.BreedNotFoundException {
         BreedFetcherForLocalTesting mock = new BreedFetcherForLocalTesting();
         assertEquals(2, Main.getNumberOfSubBreeds("hound", mock));
     }
 
     @Test
-    void getNumberOfSubBreedsInvalidBreed() {
+    void getNumberOfSubBreedsInvalidBreed() throws BreedFetcher.BreedNotFoundException {
         BreedFetcherForLocalTesting mock = new BreedFetcherForLocalTesting();
         assertEquals(0, Main.getNumberOfSubBreeds("cat", mock));
 
     }
 
     @Test
-    void exceptionTypeTest() {
+    void exceptionTypeTest() throws BreedFetcher.BreedNotFoundException {
         Exception bfe = new BreedFetcher.BreedNotFoundException("hound");
         assertTrue(bfe instanceof Exception && !(bfe instanceof RuntimeException),
                 "BreedFetcher.BreedNotFoundException must be a checked exception.");
